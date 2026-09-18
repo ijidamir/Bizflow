e824261ab5567ade23c35e2af331ce7ddc627041
Ijidamir Thomas Wazamda <wazamskid@gmail.com>
2026-09-18 18:04:31 +0100

    fix themeprovider

--- components/layout/ThemeProvider.tsx
+++ components/layout/ThemeProvider.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ComponentProps } from "react";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+  children,
+  ...props
+}: ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
